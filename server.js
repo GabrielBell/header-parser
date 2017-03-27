90b3d0e2c4d2b6ce04c62382bae4dc7ddb13380e
Gabriel Bell <gmbell23@gmail.com>
2017-03-27 20:03:40 +0000

    finished microservice

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 var express= require("express")
 var app= express()
+var path = require("path");
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+/*app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, "/index.html"))
+})*/
+
+app.get('/', function(req,res){
+    var lang= req.headers["accept-language"].split(",")[0];
+    var clientIP= req.headers["x-forwarded-for"];
+    var rawOS= req.headers["user-agent"];
+    
+    var pattern= /\(([^)]+)\)/;
+    var operatingSystem= pattern.exec(rawOS)
+    
+    res.json({"os": operatingSystem[1], "ipaddress":clientIP, "language": lang})
 })
 
 app.listen(process.env.PORT||8080, function () {
